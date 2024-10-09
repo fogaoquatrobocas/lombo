28bfd1678fa155f3c542aeec6263c066c134acd8
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2024-10-09 08:12:06 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,62 @@
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+)
+
+//Ao todo são 20 perguntas que exigem memorização
+// do seu ensino médio (qualquer disciplina)
