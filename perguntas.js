2e71f811e1e6f64b995d5c39d2f83114c6f6d6da
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2024-10-09 08:32:37 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -6,57 +6,52 @@ criaCartao(
 
 criaCartao(
     'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Qual é a fórmula para calcular a área de um quadrado?',
+    'Utiliza-se a fórmula: lado x lado'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Física',
+    'Qual a primeira lei de Newton?',
+    '1ª Lei: Um corpo em repouso permanece em repouso, e um corpo em movimento continua em movimento a menos que uma força externa atue sobre ele.'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Física',
+    'Qual a segunda lei de Newton?',
+    '2ª Lei: A força é igual à massa vezes a aceleração  (F=m.a)'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Física',
+    'Qual a terceira lei de Newton?',
+    '3ª Lei: Para cada ação, há uma reação igual e oposta.'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Física',
+    'O que é a lei da conservação da energia e quais são suas implicações?',
+    'Lei da conservação da energia: A energia não pode ser criada nem destruída, apenas transformada de uma forma para outra.'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Geografia',
+    'Qual a maior montanha do mundo',
+    'O monte Everest'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Geografia',
+    'O que é a globalização?',
+    'Integração econômica, cultural e política entre países.'
 )
 
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
+    'Geografia',
+    'O que é longitude?',
+    'É a distância a leste ou oeste do meridiano de Greenwich.'
 )
 
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1=an)*r)/2'
-)
 
 //Ao todo são 20 perguntas que exigem memorização
 // do seu ensino médio (qualquer disciplina)
